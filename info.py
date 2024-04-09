af1c933af801277a072e19249247641c07f584e8
Devpareek004 <165404806+Devpareek004@users.noreply.github.com>
2024-04-09 22:01:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,8 +45,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Admin:Admin@cluster0.yszqcak.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Admin")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Empire:Empire@cluster0.flm6drg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Empire")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter')
 
 # Others
